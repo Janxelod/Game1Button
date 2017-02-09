a81044c6bf6fa08a6866078c4a04ae8dc531faef
DESKTOP-I6S9LOU\Joan <yusuke.joan@gmail.com>
2017-02-09 17:16:48 +0100

    Create new scene with buildings, scrolling and player controller;

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+    public Camera cameraRef;
+    void Start() {
+        cameraRef = GetComponent<Camera>();
+    }
+    // Update is called once per frame
+    void Update() {
+        if (target) {
+            //if (cameraRef.transform.position.x > 1) 
+            {
+                Vector3 point = cameraRef.WorldToViewportPoint(target.position);
+                Vector3 delta = target.position - cameraRef.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+                Vector3 destination = transform.position + delta;
+                destination.x = Mathf.Clamp(destination.x, 0, 71);
+                destination.y = Mathf.Clamp(destination.y, 0, 4);// 0;
+
+
+                transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            }
+
+        }
+
+    }
+}
---
+++ Assets/PlayerTestScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestScript : MonoBehaviour {
+    public float speedX;
+    private bool moveRight;
+    private bool moveLeft;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        CheckInput();
+        movePlayer();
+    }
+    void CheckInput() {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            moveRight = true;
+            moveLeft = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            moveLeft = true;
+            moveRight = false;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow)) {
+            moveRight = false;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+            moveLeft = false;
+        }
+    }
+    void movePlayer() {
+        if(moveLeft || moveRight) {
+            float currentXposition = transform.position.x;
+            Vector3 deplacement = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+            if(moveLeft)
+                currentXposition -= (speedX * Time.deltaTime);
+            else if(moveRight)
+                currentXposition += (speedX * Time.deltaTime);
+            deplacement.x = currentXposition;
+            transform.position = deplacement;
+        }
+        
+    }
+}
