1fc5767a9bb1411be9159f86b45e39dd62716682
DESKTOP-I6S9LOU\Joan <yusuke.joan@gmail.com>
2017-02-10 15:33:30 +0100

    Added GameTitle

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject gameTitle;
+    public GameObject player;
+    public float timeToStart = 3;
+    public float currentTime;
+	// Use this for initialization
+	void Start () {
+        player.GetComponent<PlayerTestScript>().speedX = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space)) {
+            currentTime += Time.deltaTime;
+            if (currentTime > timeToStart) {
+                gameTitle.SetActive(false);
+                currentTime = 0;
+                player.GetComponent<PlayerTestScript>().speedX = 5;
+                player.GetComponent<Animator>().SetBool("GameStart", true);
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) currentTime = 0;
+	}
+}
